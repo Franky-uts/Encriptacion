e1325a636922b957ad3d92078bb944c76259e1ab
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2023-11-22 22:35:58 -0600

    Pull

--- pagina/encrypt.js
+++ pagina/encrypt.js
@@ -6,7 +6,16 @@ const boton_norm = document.getElementById('normal_btn')
 const boton_encript = document.getElementById('encryp_btn')
 const resp_norm = document.getElementById('normal_encript')
 const resp_encript = document.getElementById('encriptado_norm')
+
 var texto;
+
+const btn_encrp = document.getElementById('archivo_encrip')
+const btn_desencrp = document.getElementById('archivo_des')
+//const label_encrip = document.getElementById('nombre_archivo_encrip')
+//const label_desencrp = document.getElementById('nombre_archivo_desencrip')
+const btn_des_encrp = document.getElementById('descargar_encrip')
+const btn_des_desencrp = document.getElementById('descargar_desencrip')
+
 
 const encrypt = async () =>{
     const options = {
@@ -31,7 +40,7 @@ const unencrypt = async () =>{
     .then(response => response.json())
     .then(response =>{
         texto = response.texto
-        console.log(response.texto)
+        //console.log(response.texto)
     })
         .catch(err => console.error(err))
 }
@@ -44,4 +53,23 @@ boton_norm.addEventListener('click',async ()=>{
 boton_encript.addEventListener('click',async ()=>{
     await unencrypt()
     resp_encript.value = texto
+})
+
+btn_encrp.addEventListener('click',async () =>{
+    const referencias = await window.showOpenFilePicker({})
+    const archivo = await referencias[0].getFile();
+    const contenido = await archivo.text();		
+    console.log(contenido)
+})
+
+btn_desencrp.addEventListener('click',async () =>{
+    //alert("2")
+})
+
+btn_des_encrp.addEventListener('click',async () =>{
+    //alert("3")
+})
+
+btn_des_desencrp.addEventListener('click',async () =>{
+    //alert("4")
 })
